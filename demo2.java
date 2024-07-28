ff73ed2766a71deb5c34053bbf8f3e5233ecf6d3
primusr <primusr@R2Duo>
2024-07-28 14:37:41 +0800

    wazzup

---
+++ demo2.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+public class demo2 {
+    public static void main(String[] args) {
+        System.out.println("Java Test");
+        
+        Scanner MyVar =new Scanner(System.in);
+        System.out.println("Enter name");
+        String nme = MyVar.nextLine();
+        System.out.println("my name is" + nme);
+
+    }
+}
