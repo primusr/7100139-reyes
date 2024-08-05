ee564c8fe00e3cd8f82d27437e2a1cdc0ee48223
primusr <primus.reyes@gmail.com>
2024-08-05 10:24:38 +0800

    test

--- demo2.java
+++ demo2.java
@@ -2,7 +2,6 @@ import java.util.Scanner;
 public class demo2 {
     public static void main(String[] args) {
         System.out.println("Java Test");
-        
         Scanner MyVar =new Scanner(System.in);
         System.out.println("Enter name");
         String nme = MyVar.nextLine();
